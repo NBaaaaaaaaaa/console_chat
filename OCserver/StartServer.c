31278c5e2fd05bfb2f52708a5f1f2ad90354b3ed
Nikita Bulgin <bn.osnova@gmail.com>
2024-05-29 21:23:58 +0300

    add: online clients list
    Добавлень массив сокетов онлайн пользователей. Возможно добавлять и удалять секты из массива.

--- OCserver/StartServer.c
+++ OCserver/StartServer.c
@@ -5,6 +5,48 @@
 
 #pragma comment(lib,"ws2_32.lib")
 
+
+SOCKET *online_clients = NULL;
+size_t number_online_clients = 0;
+
+// Добавление клиента в массив онлайн пользователей
+void addClient(const SOCKET *client_socket) {
+	number_online_clients++;
+	SOCKET* temp = realloc(online_clients, number_online_clients * sizeof(SOCKET));
+	if (temp == NULL) {
+		printf("Ошибка: Не удалось выделить память\n");
+		return;
+	}
+	online_clients = temp;
+	online_clients[number_online_clients - 1] = *client_socket; // Добавляем нового клиента в конец массива
+}
+
+void delClient(const SOCKET* client_socket) {
+	// Поиск индекса пользователя по сокету
+	int index = -1;
+	for (int i = 0; i < number_online_clients; i++) {
+		if (online_clients[i] == *client_socket) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index == -1) {
+		// Добавить логирования потом здесь
+		printf("Ошибка: Пользователь не найден\n");
+		return;
+	}
+
+	// Освобождение памяти для удаленного пользователя
+	for (int i = index; i < number_online_clients - 1; i++) {
+		online_clients[i] = online_clients[i + 1]; // Сдвигаем элементы массива
+	}
+	number_online_clients--; // Уменьшаем количество пользователей
+	online_clients = realloc(online_clients, number_online_clients * sizeof(SOCKET)); // Уменьшаем размер массива
+	printf("%d", (int)number_online_clients);
+}
+
+
 int main(int argc, char* argv[])
 {
 	WSADATA wsa;
@@ -59,6 +101,7 @@ int main(int argc, char* argv[])
 	while ((new_socket = accept(s, (struct sockaddr*)&client, &c)) != INVALID_SOCKET)
 	{
 		puts("Connection accepted");
+		addClient(&new_socket);
 
 		while (TRUE) {
 			if ((recv_size = recv(new_socket, client_reply, 2000, 0)) == SOCKET_ERROR)
@@ -77,6 +120,9 @@ int main(int argc, char* argv[])
 				// Обработка ошибки или прерывания соединения
 				// Например:
 				puts("Error: Invalid recv_size or buffer overflow occurred");
+
+				delClient(&new_socket);
+				closesocket(new_socket);
 				return 1;
 			}
 
@@ -85,6 +131,8 @@ int main(int argc, char* argv[])
 			//send(new_socket, message, strlen(message), 0);
 	
 		}
+
+		
 	}
 
 	if (new_socket == INVALID_SOCKET)
